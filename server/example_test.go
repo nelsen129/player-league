533b08341424c3e5d43a1b4d7b29cc30e7661bed
Patrick Nelsen <55818910+nelsen129@users.noreply.github.com>
2024-10-02 21:26:46 -0700

    fix: server example

--- server/example_test.go
+++ server/example_test.go
@@ -9,18 +9,24 @@ import (
 	"github.com/nelsen129/player-league/store"
 )
 
-func ExamplePlayerServer_ServeHTTP() {
+func ExamplePlayerServer() {
 	playerStore := store.NewInMemoryPlayerStore()
 	playerServer := server.NewPlayerServer(playerStore)
 
 	postRequest, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
 	getRequest, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+	leagueRequest, _ := http.NewRequest(http.MethodGet, "/league", nil)
 
 	playerServer.ServeHTTP(httptest.NewRecorder(), postRequest)
 
 	response := httptest.NewRecorder()
 	playerServer.ServeHTTP(response, getRequest)
-
 	fmt.Println(response.Body.String())
-	// Output: 1
+
+	response = httptest.NewRecorder()
+	playerServer.ServeHTTP(response, leagueRequest)
+	fmt.Println(response.Body.String())
+	// Output:
+	// 1
+	// [{"name":"Pepper","wins":1}]
 }
